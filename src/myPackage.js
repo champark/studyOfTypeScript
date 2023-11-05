0811261f6ead3cf5f55aacc753d384b50002c415
champark <97137143+champark@users.noreply.github.com>
2023-11-06 00:58:41 +0900

    finish

---
+++ src/myPackage.js
@@ -0,0 +1,19 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {Object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) {
+    return true;
+}
+/**
+ * Exists the program
+ * @param {number} code 
+ * @returns number
+ */
+export function exit(code) {
+    return code + 1;
+}
